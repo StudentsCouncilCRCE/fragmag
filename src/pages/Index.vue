9027f60e1269feeba76af1dc4a74a6b0dfad0abf
Ryan Dsilva <ryan.dsilva.98@gmail.com>
2020-07-18 18:54:47 +0530

    demo

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,42 +1,7 @@
 <template>
-  <Layout :show-logo="false">
-    <!-- List articles -->
-    <div class="articles">
-      <ArticleCard
-        v-for="edge in $page.articles.edges"
-        :key="edge.node.id"
-        :article="edge.node"
-      />
-    </div>
-  </Layout>
+  <div>
+    <h1>Index</h1>
+  </div>
 </template>
 
-<page-query>
-query {
-  articles: allArticle(filter: { published: { eq: true }}) {
-    edges {
-      node {
-        id
-        title
-        timeToRead
-        description
-        cover_image (width: 770, height: 380, blur: 10)
-        path
-      }
-    }
-  }
-}
-</page-query>
-
-<script>
-import ArticleCard from "~/components/ArticleCard.vue";
-
-export default {
-  components: {
-    ArticleCard,
-  },
-  metaInfo: {
-    title: "Hello, world!",
-  },
-};
-</script>
+<style scoped lang="scss"></style>
