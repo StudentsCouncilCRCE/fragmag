e34df5c96cbaca9b19cbd85ff013b80a96499563
sakshi ghadigaonkar <sakshig2507@gmail.com>
2020-07-21 12:51:25 +0530

    episode-2 update

---
+++ src/pages/episode-2/achievers-club.vue
@@ -0,0 +1,87 @@
+<template>
+  <Layout>
+    <h1 class="p-1 text-center">ACHIEVERS CLUB</h1>
+    <br />
+    <div class="container">
+      <ul>
+        <li>
+          TEAM ABADHA:
+          <br />All India Rank 5, out of 82 teams, at SAE BAJA 2020, held at Chandigarh
+        </li>
+        <br />
+        <li>
+          TEAM CRCE FORMULA RACING:
+          <br />Secured an overall rank of 25 amongst 90 participating teams in the Formula
+          Bharat 2020 held at the Kari Motor speedway, Coimbatore.
+        </li>
+        <br />
+        <li>
+          TEAM VAAYUSHASTRA:
+          <br />Secured 9 th position in the design report and overall 10 th position globally at the
+          SAE Aero Design (East) competition in the Advanced class category held at
+          Texas, USA in March 2020.
+        </li>
+        <br />
+        <li>
+          TEAM MAVERICKS:
+          <br />Received the ‘Just Joe Brannan Sportsmanship Award’ at the SUAS 2019-
+          Student Unmanned Aerial system held in Maryland, USA in June 2019.
+        </li>
+        <br />
+        <li>
+          Mr. Vedant Sahi (TE Computer)was member of the team that won the 5 th position at the Singapore-India
+          Hackathon, held at IIT Madras in September 2019. The team won the ‘Prize of
+          Encouragement’ award for idea with potential
+        </li>
+        <br />
+        <li>
+          Mr. Noronha Alden Anil (TE Electronics) won gold medal in the 110m hurdles)
+          at the Khelo India Youth Games 2019 at Pune. He also won gold medal (with a
+          timing of 14.267sec) at the All India Inter-University Athletics Championship
+          in Jan 2020.
+        </li>
+        <br />
+        <li>
+          Mr. Dhruvil Mataliya was selected to participate in the Asia Youth International
+          Model United Nations at Putrajaya, Malaysia in August 2019
+        </li>
+        <br />
+        <li>
+          CRCE FOOTBALL TEAM:
+          <br />
+          <ul>
+            <li>
+              1 st in Inter College Football Tournament organized by KJ Somaiya Medical
+              College and Research Centre.
+            </li>
+            <li>
+              Mr. Erhard Fernandes ( TE PRODUCTION) was awarded as the Player of the
+              Tournament
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </Layout>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.container {
+  background-color: #0f2d44;
+  padding-left: 40px;
+  padding-right: 40px;
+  padding-top: 40px;
+  padding-bottom: 40px;
+  text-align: justify;
+}
+ul {
+  list-style: inside;
+}
+ul li {
+  font: bold;
+}
+</style>
--- src/pages/episode-2/hall-of-fame.vue
+++ src/pages/episode-2/hall-of-fame.vue
@@ -1,6 +1,229 @@
 <template>
   <Layout>
-    <h1>Hello</h1>
+    <h1 class="p-1 text-center">HALL OF FAME</h1>
+    <br />
+    <div class="container">
+      <table>
+        <thead>
+          <tr>
+            <th>EXAMINATION</th>
+            <th>RANK</th>
+            <th>STUDENT NAME</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td rowspan="3">First year</td>
+            <td>1</td>
+            <td>Biswas Simran Amit</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Mascarenhas Nisha Nitin</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Dabre Chelsea Moses</td>
+          </tr>
+          <tr>
+            <td rowspan="3">
+              SECOND YEAR
+              (PRODUCTION)
+            </td>
+            <td>1</td>
+            <td>Sharma Tushant Prashaant</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Purav Advait Chetan</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Haldankar Priyam Rajiv</td>
+          </tr>
+          <tr>
+            <td rowspan="3">
+              SECOND YEAR
+              (ELECTRONICS)
+            </td>
+            <td>1</td>
+            <td>Kar Sumanto Ashim</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Kumar Yash Ramesh</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Dhingra Urvashi Vikram</td>
+          </tr>
+          <tr>
+            <td rowspan="3">
+              SECOND YEAR
+              (COMPUTER)
+            </td>
+            <td>1</td>
+            <td>Menezes Elita Elroy</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Kadamala Kevlyn Stanly</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Sahai Vedant Sanjeev</td>
+          </tr>
+          <tr>
+            <td rowspan="3">
+              SECOND YEAR
+              ((INFORMATION TECH)
+            </td>
+            <td>1</td>
+            <td>Machado Karen Felix</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Chendekar Tejas Prasad</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Tanwar Priya Narendra</td>
+          </tr>
+          <tr>
+            <td rowspan="3">
+              THIRD YEAR
+              (PRODUCTION)
+            </td>
+            <td>1</td>
+            <td>Khatu Riddhesh Digambar</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Kale Gandhar Sudesh</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Chavan Anurag Satish</td>
+          </tr>
+          <tr>
+            <td rowspan="3">
+              THIRD YEAR
+              (ELECTRONICS)
+            </td>
+            <td>1</td>
+            <td>Sheikh Aman Ahesan</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Pal Ashishkumar Devidas</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Poddar Gautam D</td>
+          </tr>
+          <tr>
+            <td rowspan="4">
+              THIRD YEAR
+              (COMPUTER)
+            </td>
+            <td>1</td>
+            <td>Sakhardande Vedant A</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Checker Juhi Vipi</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Mathias Jenell Herald</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Payapilly Merlin Kuruvilla</td>
+          </tr>
+          <tr>
+            <td rowspan="3">
+              THIRD YEAR
+              (INFORMATION TECH)
+            </td>
+            <td>1</td>
+            <td>Dsouza Lerisha Janice J</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Dcosta Arlene Antony</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Singh Anuj Kumar</td>
+          </tr>
+          <tr>
+            <td rowspan="3">
+              FINAL YEAR
+              (PRODUCTION)
+            </td>
+            <td>1</td>
+            <td>Kadam Shwet Milind</td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>Rodrigues Vanessa Cletus</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Dedhia Yash Ketan</td>
+          </tr>
+          <tr>
+            <td rowspan="3">
+              FINAL YEAR
+              (ELECTRONICS)
+            </td>
+            <td>1</td>
+            <td>Dsouza Austin Albert</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Chaudhari Rohan D</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Ranim Tanvi Suhas</td>
+          </tr>
+          <tr>
+            <td rowspan="3">
+              FINAL YEAR
+              (COMPUTER)
+            </td>
+            <td>1</td>
+            <td>Borkar Pradnya K</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Pulinthitta Marilyn M</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Gharat Anisha Milind</td>
+          </tr>
+          <tr>
+            <td rowspan="3">
+              FINAL YEAR
+              (INFORMATION TECH)
+            </td>
+            <td>1</td>
+            <td>Rathod Vikramsingh K</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Shah Priyank Sanjay</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Shetty Suparna Sudhakar</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </Layout>
 </template>
 
@@ -8,4 +231,30 @@
 export default {};
 </script>
 
-<style></style>
+<style>
+.container {
+  background-color: #0f2d44;
+  padding-left: 40px;
+  padding-right: 40px;
+  padding-top: 40px;
+  padding-bottom: 40px;
+  text-align: justify;
+}
+ul {
+  list-style: inside;
+}
+ul li {
+  font: bold;
+}
+table {
+  border-collapse: collapse;
+  width: 100%;
+}
+
+th,
+td {
+  padding: 8px;
+  text-align: LEFT;
+  border-bottom: 1px solid #ddd;
+}
+</style>
