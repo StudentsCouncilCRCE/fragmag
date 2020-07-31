4effb38d1f21d7b0bc2f00bbdbfd1e391f37cb97
sakshi ghadigaonkar <sakshig2507@gmail.com>
2020-08-01 00:04:23 +0530

    BE PROD One-liners Updated

--- src/pages/episodes/5/liners.vue
+++ src/pages/episodes/5/liners.vue
@@ -3,8 +3,6 @@
     <h1 class="p-1 text-center">BE One-Liners</h1>
     <div class="pt-4 grid grid-cols-1 lg:grid-cols-2 gap-4">
       <LinkCard :link="comps"></LinkCard>
-      <LinkCard :link="it"></LinkCard>
-      <LinkCard :link="elex"></LinkCard>
       <LinkCard :link="prod"></LinkCard>
     </div>
   </Layout>
@@ -22,15 +20,6 @@ export default {
       comps: {
         title: "BE Comps",
         path: "/episodes/5/comps",
-      },
-      it: {
-        title: "BE IT",
-        path: "/episodes/5/it",
-      },
-
-      elex: {
-        title: "BE Elex",
-        path: "/episodes/5/elex",
       },
 
       prod: {
--- src/pages/episodes/5/prod.vue
+++ src/pages/episodes/5/prod.vue
@@ -2,11 +2,7 @@
   <Layout :show-logo="true">
     <h1 class="p-1 text-center">BE Production</h1>
     <div class="content pt-4 grid grid-cols-1 md:grid-cols-3 gap-4">
-      <BECard
-        v-for="person in $page.prod.edges"
-        :key="person.node.id"
-        :person="person.node"
-      />
+      <BECard v-for="person in $page.prod.edges" :key="person.node.id" :person="person.node" />
     </div>
   </Layout>
 </template>
